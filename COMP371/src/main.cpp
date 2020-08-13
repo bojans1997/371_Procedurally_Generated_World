02da9863363d01e1ff006f494235c3777f8a65df
DESKTOP-3IMGBC4\Bojan Srbinoski <b2thes@hotmail.com>
2020-08-13 12:33:06 -0400

    allow camera to look up without moving vertically

--- COMP371/src/main.cpp
+++ COMP371/src/main.cpp
@@ -84,7 +84,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
 	glm::vec3 front;
 	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-	front.y = sin(glm::radians(0.0f));
+	front.y = sin(glm::radians(pitch));
 	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
 	cameraFront = glm::normalize(front);
 }
@@ -185,24 +185,28 @@ void processInput(GLFWwindow* window)
 
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
 		glm::vec3 newCameraPos = cameraPos + cameraSpeed * cameraFront;
+		newCameraPos.y = 2.0f;
 		if (!checkCollision(trees, newCameraPos)) {
 			cameraPos = newCameraPos;
 		}
 	}
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
 		glm::vec3 newCameraPos = cameraPos - cameraSpeed * cameraFront;
+		newCameraPos.y = 2.0f;
 		if (!checkCollision(trees, newCameraPos)) {
 			cameraPos = newCameraPos;
 		}
 	}
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
 		glm::vec3 newCameraPos = cameraPos - glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		newCameraPos.y = 2.0f;
 		if (!checkCollision(trees, newCameraPos)) {
 			cameraPos = newCameraPos;
 		}
 	}
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
 		glm::vec3 newCameraPos = cameraPos + glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		newCameraPos.y = 2.0f;
 		if (!checkCollision(trees, newCameraPos)) {
 			cameraPos = newCameraPos;
 		}
